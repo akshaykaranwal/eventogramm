ddde4c1b122ad176690586bc793e4a6a0fd36733
Akshay <akshaykaranwal6@gmail.com>
2022-12-08 20:39:02 +0530

    backend added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const bp = require("body-parser");
 const express = require("express")
+const mondb = require("mongoose")
 
 
 const app = express();
@@ -15,7 +16,7 @@ app.get("/signin",function(req,res){
 });
 
 app.post("/signin",function(req,res){
-    var name = req.body.name;
+    var namee = req.body.name;
     var email = req.body.email;
     var pasw = req.body.password;
     var cpasw = req.body.confirm-password;
@@ -24,6 +25,14 @@ app.post("/signin",function(req,res){
 app.get("/login",function(req,res){
     res.sendFile(__dirname + "/login.html");
 });
+
+app.get("/about",function(req,res){
+    res.sendFile(__dirname + "/about.html");
+});
+
+
+//mondb.connect("mongodb://0.0.0.0:27017/eventodb",{useNewUrlParser:true});
+
 
 app.listen(3000,function(){
     console.log("Server started at port 3000")
